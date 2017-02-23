d4021427f515fd4d614aa751268be309316925bf
BenzLeung <lwbin3795@gmail.com>
2017-02-24 02:18:47 +0800

    developing

--- js/requireMain.js
+++ js/requireMain.js
@@ -12,17 +12,17 @@ require.config({
     baseUrl: 'js',
     paths: {
         cocos: '../lib/cocos2d-js-v3.13-custom',
-        ccui: '../lib/cocos2d-ccui',
+        //ccui: '../lib/cocos2d-ccui',
         chipmunk: '../lib/chipmunk'
     },
     shim: {
         cocos: {
             exports: 'cc'
         },
-        ccui: {
+        /*ccui: {
             deps: ['cocos'],
             exports: 'ccui'
-        },
+        },*/
         chipmunk: {
             exports: 'cp'
         }
